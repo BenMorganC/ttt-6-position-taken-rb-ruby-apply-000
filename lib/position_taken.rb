eb3711583a3e361db4221d732a25f78cef7f4f00
Benjamin Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-02 23:12:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board,index)
 end
 
-  if board[index] == " " || board[index] == "" || board[index] == nil
+if board[index] == " " || board[index] == "" || board[index] == nil
   return false 
   else 
   return true
